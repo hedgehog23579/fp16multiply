91a7c98ecb0ccfaa15cbe61128eac2a8c158abf3
hedgehog23579 <32409452+hedgehog23579@users.noreply.github.com>
2020-08-09 22:23:21 +0800

    Update 16mul.py

--- 16mul.py
+++ 16mul.py
@@ -23,17 +23,10 @@ def mul16(a,b):
   exp_b = (b&exp) >> 10
   frac_b = (b&frac) + 1024
 
- #print('frac_a=',bin(frac_a)[2:])
- #print('frac_b=',bin(frac_b)[2:])
- #print('exp_a=',bin(exp_a)[2:])
- #print('exp_b=',bin(exp_b)[2:])
-
  sign_o = sign_a ^ sign_b
  exp_o = exp_a + exp_b
  product = frac_a * frac_b
- #print('product=',bin(product)[2:])
- #print('exp_o=',exp_o)
- #print('product & 0x100000=',bin(product & 0x100000))
+
  if(product&0x200000):  #20000?
   exp_o = exp_o + 1
   product = product >> 1
@@ -48,9 +41,9 @@ def mul16(a,b):
  exp_o = exp_o - 15
  if(exp_o == 0):
   product = product >> 1
- #print('product=',bin(product)[2:])
+
  frac_o = (product & 0x1ffc00) >> 10
- #print('frac_o=',bin(frac_o)[2:])
+
  if(product & 0x200):
   frac_o = frac_o + 1
  sign_o = sign_o << 15
@@ -58,11 +51,3 @@ def mul16(a,b):
  sign_o = sign_o | exp_o | frac_o
  o = sign_o
  return o
-'''
- if __name__ == '__main__':
- ##print('bb')
- ans = mul16('1100000000011100','1011111110110011')
- print('ans = ',bin(ans)[2:].zfill(16))
- print('true ans=','0100001111101001')
- ##print(hex(-2.0),hex(-3.0))
-'''
